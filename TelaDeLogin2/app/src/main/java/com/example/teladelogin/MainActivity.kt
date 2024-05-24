2bb3637b6d91ea0e7baa0e758b326b68e92f0fd1
caihumm <fortescaique@gmail.com>
2024-05-24 16:11:39 -0300

    tela de login

---
+++ TelaDeLogin2/app/src/main/java/com/example/teladelogin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.teladelogin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.et_username)
+        etPassword = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Porfavor coloque o usuario e a senha.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Logado com sucesso.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
